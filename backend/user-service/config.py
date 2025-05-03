ad0ad24493ce146071b8608079e651052f7c35cd
hanine Benali <100847640+hanyne@users.noreply.github.com>
2025-05-03 11:11:21 +0200

    Update config.py

--- backend/user-service/config.py
+++ backend/user-service/config.py
@@ -1,6 +1,6 @@
 class Config:
     DEBUG = False
     SECRET_KEY = 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Nothing123@db:5432/reservation_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:@db:5432/reservation_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     KAFKA_BOOTSTRAP_SERVERS = 'kafka:9092'
